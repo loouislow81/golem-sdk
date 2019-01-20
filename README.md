350b55188c7af1bb2cc1b74fa28694509e6408f4
Loouis Low <loouis@gmail.com>
2019-01-20 13:09:58 +0800

    restyling generic login page

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# golem-sdk 2.0.48
+# golem-sdk 2.0.50
 
 SDK for GOLEM CLI and GOLEM App Store to craft Linux desktop apps.
 
--- app/lib/static/login/login.js
+++ app/lib/static/login/login.js
@@ -1,20 +1,12 @@
-'use strict';
+import electron from 'electron';
 
-var _electron = require('electron');
+const { ipcRenderer } = electron;
 
-var _electron2 = _interopRequireDefault(_electron);
+const form = document.getElementById('login-form');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var ipcRenderer = _electron2.default.ipcRenderer;
-
-
-var form = document.getElementById('login-form');
-
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  var username = document.getElementById('username-input').value;
-  var password = document.getElementById('password-input').value;
+  const username = document.getElementById('username-input').value;
+  const password = document.getElementById('password-input').value;
   ipcRenderer.send('login-message', [username, password]);
 });
-//# sourceMappingURL=login.js.map
